a60a6452830b0d8999c45306643e2c1100146b1d
Caitlin Cradick <caitlincradick@gmail.com>
2023-08-01 15:37:10 -0700

    feat/apiCalls and navBar

--- src/components/JokeContainer/JokeContainer.js
+++ src/components/JokeContainer/JokeContainer.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+import { getJokes, getCat } from '../../apiCalls'
+import Joke from '../Joke/Joke'
+import './JokeContainer.css'
 
 const JokeContainer = () => {
+ const [joke, setJoke] = useState("")
+const [cat, setCat] = useState("")
+
+console.log(cat)
+
+useEffect(() => {
+  getCat()
+    .then(data => {
+      setCat(data.url)
+    })
+}, [])
+
+useEffect(() => {
+  getJokes()
+    .then(data => {
+      setJoke(data)
+    })
+    //add .catch for error
+}, [])
+
   return (
-    <div>JokeContainer</div>
+    <div className ='joke-container'>
+      <img src={cat}></img>
+      <div className= 'joke'>
+     <Joke joke={joke} />
+     </div>
+    </div>
   )
 }
 
