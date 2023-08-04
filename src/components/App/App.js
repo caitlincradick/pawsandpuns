a0ee85e8bf347e4f3109662ef4322d4f0ef9c474
Caitlin Cradick <caitlincradick@gmail.com>
2023-08-03 20:50:17 -0700

    feat/add error handling routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,8 +3,10 @@ import {useState, useEffect} from 'react'
 import { getJokes, getCat } from '../../apiCalls';
 import JokeContainer from '../JokeContainer/JokeContainer';
 import NavBar from '../NavBar/NavBar';
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import SavedJokes from '../SavedJokes/SavedJokes';
+import PageNotFound from '../PageNotFound/PageNotFound';
+
 
 function App() {
 const [savedList, setSavedList] = useState([])
@@ -30,11 +32,12 @@ const [saved, setSaved] = useState(false)
   <Routes>
     <Route path = "/" element = {
       <JokeContainer  savedList={savedList} setSavedList={setSavedList} addSaved={addSaved} joke={joke} setJoke={setJoke} saved={saved} setSaved={setSaved}/>
-      } />
+      }/>
       <Route path= "/saved" element= {
         <SavedJokes savedList={savedList} setSavedList={setSavedList} joke={joke} setJoke={setJoke} saved={true} setSaved={setSaved}/>
-      }
-      />
+      }/>
+      <Route path ="/404" element={<PageNotFound />}/>
+      <Route path="*" element={<Navigate to= "/404" />} />
     </Routes>
     </div>
   );
