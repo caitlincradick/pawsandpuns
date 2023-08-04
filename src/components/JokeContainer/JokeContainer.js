b39abeb0d55e0400f467d784cf2d638060c0b368
Caitlin Cradick <caitlincradick@gmail.com>
2023-08-03 20:13:42 -0700

    feat/add error handling for cat gif

--- src/components/JokeContainer/JokeContainer.js
+++ src/components/JokeContainer/JokeContainer.js
@@ -5,18 +5,30 @@ import Joke from '../Joke/Joke'
 import './JokeContainer.css'
 
 const JokeContainer = ({ savedList, setSavedList, addSaved, joke, setJoke, saved, setSaved}) => {
-  // const [saved, setSaved] = useState(false)
-// const [joke, setJoke] = useState("")
 const [cat, setCat] = useState("")
+const [catLoading, setCatLoading] = useState(false)
+const [catErr, setCatErr] = useState("")
+const [jokeLoading, setJokeLoading] = useState("")
 
 console.log('ID', joke.id)
 
 
 useEffect(() => {
+  setCatLoading(true)
   getCat()
-    .then(data => {
-      setCat(data.url)
-    })
+    .then(data => setCat(data.url))
+    
+      .catch(catErr => {
+        if(catErr === 500) {
+          setCatErr('Oh no, looks there is a server error!')
+      } else {
+        setCatErr(catErr)
+      }
+      })
+      .finally(() => {
+        setCatLoading(false)
+      })
+    // })
 }, [])
 
 useEffect(() => {
@@ -28,9 +40,13 @@ useEffect(() => {
     //add .catch for error
 }, [])
 
+if(catErr){
+  return <h1 className='err-message'>An error has occured: {catErr}</h1>
+}
+
   return (
     <div className ='joke-container'>
-      <img className='gif' src={cat} alt="cat-gif"></img>
+      {catLoading ? <h1 className='err-message'>Loading Cats....</h1> : <img className='gif' src={cat} alt="cat-gif"></img>}
       <div className= 'joke'>
      <Joke id={joke.id} joke={joke} saved={saved} setSaved={setSaved} savedList={savedList} setSavedList={setSavedList} addSaved={addSaved} />
      </div>
