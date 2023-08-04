b980f4a40dfdcdad687818861704b4e46e23b0e0
Caitlin Cradick <caitlincradick@gmail.com>
2023-08-03 20:56:10 -0700

    feat/add CSS stying

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import "../../sad-kitty.png"
+import "./PageNotFound.css"
 
 const PageNotFound = () => {
   return (
-    <div className = 'page not found'>
+    <div className = 'page-not-found'>
       <h1>Sorry.. 404 error, page not found.</h1>
       <img src={require('../../sad-kitty.png')}></img>
     </div>
